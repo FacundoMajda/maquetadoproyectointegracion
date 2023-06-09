e3ee1016338d40561bb2dd19959db670b032ff01
stormbreaker20 <105421659+stormbreaker20@users.noreply.github.com>
2023-06-09 02:19:39 -0300

    se agregó nodemon y morgan para registrar todas las peticiones al backend

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-import express from 'express';
+import express from "express";
+
+//funcionalidad para registrar todas las peticiones con un mensaje por consola
+//'node run dev'
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 
-app.listen(3000)
-console.log('server listening on port 3000');
+app.listen(3000);
+console.log("server listening on port 3000");
 
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import app from "./app.js";
+//inicio del servidor
+
+import app from './app.js';
+
+
+
 app.listen(3000);
+
+
 console.log("server listening on port 3000");
