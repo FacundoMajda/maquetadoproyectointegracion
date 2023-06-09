c27f10661e9a5e8cfaa1907a38d82f5f41096a89
stormbreaker20 <105421659+stormbreaker20@users.noreply.github.com>
2023-06-09 03:01:54 -0300

    se importó authroutes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ import express from "express";
 //'node run dev'
 import morgan from "morgan";
 
+
+//import de rutas
+import authRoutes from "./routes/auth.routes.js"
+
 const app = express();
+
 app.use(morgan("dev"));
 
+app.use(authRoutes);
 
 export default app;
