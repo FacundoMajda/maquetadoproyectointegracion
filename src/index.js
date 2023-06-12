3134fbf9264f812270064f5e6fecaeba117501ac
FacundoMajda <facundomajda13@gmail.com>
2023-06-12 11:53:47 -0300

    vuelve a funcionar?

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-//inicio del servidor
-
+//Index.js ejecuta el servidor, importando funcionalidad de app.js
 import app from "./app.js";
-
+//Import base de datos definida en db.js
 import { connectDB } from "./db.js";
 
+//Modulo nodemon para volver a iniciar el servidor automaticamente luego de cada cambio
+import nodemon from "nodemon";
+
+//Iniciamos la base de datos
 connectDB();
 
-app.listen(3000);
-
-console.log("server listening on port 3000");
+//Modulo para iniciar servidor en puerto indicado
+app.listen(4000);
+console.log("listening on  port", 4000);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,29 +1,17 @@
-//especificacion a mongo db de como guardar los datos
-//esquema
-//funcion para crear estructura
+import mongoose from "mongoose";
 
-import mongoose, { mongo } from "mongoose";
-
-//definimos y exportamos esquema de usuario
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
     trim: true,
   },
 
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
+  email: { type: String, required: true, trim: true, unique: true },
 
-  password: {
-    type: String,
-    required: true,
-  },
+  password: { type: String, required: true },
 });
 
-//exportamos
 export default mongoose.model("User", userSchema);
+
+//
